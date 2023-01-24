1518f694c83a12d8f36abb14a60b924dfcd2f7e3
sodiqparamadina <sodiqardianto@gmail.com>
2023-01-24 16:08:16 +0700

    Update Logo
    Menampikan logo di login, dan dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,7 +52,7 @@
                 <!-- CONTAINER OPEN -->
                 <div class="col col-login mx-auto mt-7">
                     <div class="text-center">
-                        <img src="{{ asset('assets/images/brand/logo-white.png') }}" class="header-brand-img" alt="">
+                        <img src="{{ asset('assets/images/brand/logo-kumnamu.png') }}" class="header-brand-img" alt="">
                     </div>
                 </div>
 
